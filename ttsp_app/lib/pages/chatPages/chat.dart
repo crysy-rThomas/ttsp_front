735ed794bdd507f8e36b4ae5a136c29da25abd60
Ryan THOMAS <ryan.thomas39500@gmail.com>
2024-07-23 10:03:00 +0200

    add chatbox

--- ttsp_app/lib/pages/chatPages/chat.dart
+++ ttsp_app/lib/pages/chatPages/chat.dart
@@ -3,35 +3,89 @@ import 'package:flutter/material.dart';
 
 import '../../main.dart';
 GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
 class Chat extends StatefulWidget {
   const Chat({super.key});
 
   @override
-  State<Chat> createState() => _Chat();
+  State<Chat> createState() => _ChatState();
 }
 
-class _Chat extends State<Chat> with WidgetsBindingObserver {
+class _ChatState extends State<Chat> with WidgetsBindingObserver {
+  final List<String> _messages = [];
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _messages.add(_controller.text);
+        _controller.clear();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          color: Colors.blueAccent,
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Text(pos.toString(), textScaleFactor: 10.0),
-                ElevatedButton(
-                  child: const Text('Go To Page of index 1'),
-                  onPressed: () {
-                    //Page change using state does the same as clicking index 1 navigation button
-                    final CurvedNavigationBarState? navBarState =
-                        _bottomNavigationKey.currentState;
-                    navBarState?.setPage(1);
-                  },
-                )
-              ],
+        color: Colors.blueAccent,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Text(
+                      _messages[index],
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ));
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Enter your message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.send),
+                    color: Colors.white,
+                    onPressed: _sendMessage,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
