e36fc6fde220a074192f9f4b4790c683bb0b77cb
Ryan THOMAS <ryan.thomas39500@gmail.com>
2024-07-23 22:28:51 +0200

    document creat + get

--- ttsp_app/lib/pages/documentPages/document.dart
+++ ttsp_app/lib/pages/documentPages/document.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../models/document.dart';
 import '../../services/document_service.dart';
 
@@ -13,6 +12,7 @@ class Documents extends StatefulWidget {
 class _DocumentState extends State<Documents> {
   final DocumentService _documentService = DocumentService();
   List<Document> documents = [];
+  Map<String, String> documentContents = {};
 
   @override
   void initState() {
@@ -27,11 +27,68 @@ class _DocumentState extends State<Documents> {
     });
   }
 
-  void _addNewDocument() {
-    // For simplicity, adding a new document with a timestamp
-    setState(() {
-      // documents.add();
+  void _addNewDocument(String title, String content) {
+    setState(() async {
+      Document newDocument = await _documentService.addDocument(title, content);
+      documents.add(newDocument);
     });
+  }
+
+  Future<void> _loadDocumentContent(String id) async {
+    String content = await _documentService.getDocumentContent(id);
+    setState(() {
+      documentContents[id] = content;
+    });
+  }
+
+  void _showAddDocumentDialog() {
+    String title = '';
+    String content = '';
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add New Document'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                onChanged: (value) {
+                  title = value;
+                },
+              ),
+              SizedBox(
+                height: 200, // Adjust the height as needed
+                width: double.infinity, // Makes the TextField take the full width of its parent
+                child: TextField(
+                  maxLines: null, // Makes the TextField expandable or you can set a specific number
+                  decoration: const InputDecoration(labelText: 'Content'),
+                  onChanged: (value) {
+                    content = value;
+                  },
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Ajouter'),
+              onPressed: () {
+                _addNewDocument(title, content);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -42,22 +99,33 @@ class _DocumentState extends State<Documents> {
         child: ListView.builder(
           itemCount: documents.length,
           itemBuilder: (context, index) {
+            var document = documents[index];
             return Container(
-              margin:
-                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+              margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
               padding: const EdgeInsets.all(10.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10.0),
               ),
               child: ExpansionTile(
-                title: Text(documents[index]
-                    .title), // Assuming documents[index] has a title property
+                title: Text(document.title),
                 children: <Widget>[
                   Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: Text(documents[index]
-                        .content), // Assuming documents[index] has a content property
+                    child: documentContents.containsKey(document.id)
+                        ? Text(documentContents[document.id]!)
+                        : FutureBuilder(
+                            future: _loadDocumentContent(document.id),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return const CircularProgressIndicator();
+                              } else if (snapshot.hasError) {
+                                return Text('Error: ${snapshot.error}');
+                              } else {
+                                return Text(documentContents[document.id] ?? 'No content');
+                              }
+                            },
+                          ),
                   ),
                 ],
               ),
@@ -66,9 +134,9 @@ class _DocumentState extends State<Documents> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addNewDocument,
+        onPressed: _showAddDocumentDialog,
         tooltip: 'Add Document',
-        child: Icon(Icons.add, color: Colors.white),
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
--- ttsp_app/lib/services/document_service.dart
+++ ttsp_app/lib/services/document_service.dart
@@ -16,20 +16,78 @@ class DocumentService {
           },
         ),
       );
-    if (response.statusCode == 200) {
-      final List<Document> documents = [];
-      for (final doc in response.data) {
-        documents.add(Document(
-          id: doc['id'],
-          title: doc['name'],
-          content: "test",
-        ));
+      if (response.statusCode == 200) {
+        final List<Document> documents = [];
+        for (final doc in response.data) {
+          documents.add(Document(
+            id: doc['id'],
+            title: doc['name'],
+            content: "test",
+          ));
+        }
+        return documents;
       }
-      return documents;
-    }
     } catch (e) {
       print(e);
     }
     throw Exception('Failed to get documents');
+  }
+
+  Future<String> getDocumentContent(String id) async {
+    const FlutterSecureStorage storage = FlutterSecureStorage();
+    final String? token = await storage.read(key: 'access_token');
+
+    try {
+      var response = await Dio().get(
+        'http://127.0.0.1:8000/v0/document/$id/content',
+        options: Options(
+          headers: {
+            "Authorization": "Bearer $token",
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      }
+    } catch (e) {
+      print(e);
+    }
+    throw Exception('Failed to get document content');
+  }
+
+  Future<Document> addDocument(String title, String content) async {
+    const FlutterSecureStorage storage = FlutterSecureStorage();
+    final String? token = await storage.read(key: 'access_token');
+
+    try {
+      var response = await Dio().post(
+        'http://127.0.0.1:8000/v0/document/manual',
+        data: {
+          'name': title,
+          'content': content,
+        },
+        options: Options(
+          headers: {
+            "Authorization": "Bearer $token",
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return Document(
+          id: response.data['id'],
+          title: response.data['name'],
+          content: response.data['content'],
+        );
+      } else {
+        // Handle non-200 responses
+        print('Server responded with status code: ${response.statusCode}');
+        print('Response data: ${response.data}');
+        throw Exception('Failed to add document');
+      }
+    } catch (e) {
+      // Catch and print any errors for debugging
+      print('Error adding document: $e');
+      rethrow; // Rethrow the error to be handled by the caller
+    }
   }
 }
